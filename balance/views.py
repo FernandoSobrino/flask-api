b3c008ba75cfa09c1cab14c59fd28a53a673d110
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-14 21:48:23 +0200

    primer endpoint de movimientos

--- balance/views.py
+++ balance/views.py
@@ -1,9 +1,26 @@
-from distutils.command.config import config
+from flask import jsonify
 from . import app
+from .models import DBManager
 
+"""
+Verbos y formato de endpoints
+------------------------------
+GET /movimientos ---> LISTA movimientos
+POST /movimientos ---> CREAR movimiento
+GET /movimientos/1 --> LEER el movimiento con ID 1
+POST /movimientos/1 -> ACTUALIZAR el movimiento con ID 1 (sobreescribe todo el objeto)
+PUT /movimientos/1 --> ACTUALIZAR el movimiento con ID 1 (sobreescribe parcialmente)
+DELETE /movimientos/1 -> ELIMINAR el movimiento con ID 1
 
-@app.route("/")
-def inicio():
-    return (f"La ruta del archivo de datos es:"
-            f"{app.config['RUTA']}<br>Secret Key: "
-            f"{app.config['SECRET_KEY']}")
+importante versionar los endpoints (son un contrato)
+/apo/v1/...
+"""
+
+RUTA = app.config.get("RUTA")
+
+@app.route("/api/v1/movimientos")
+def listar_movimientos():
+    db = DBManager(RUTA)
+    sql = "SELECT * from movimientos ORDER BY fecha, id"
+    movimientos = db.consultaSQL(sql)
+    return jsonify(movimientos)
