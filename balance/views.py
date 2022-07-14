d772217581537e216c750ade664f14ccaa9abc8c
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-14 21:28:23 +0200

    feat: esqueleto de la api

---
+++ balance/views.py
@@ -0,0 +1,9 @@
+from distutils.command.config import config
+from . import app
+
+
+@app.route("/")
+def inicio():
+    return (f"La ruta del archivo de datos es:"
+            f"{app.config['RUTA']}<br>Secret Key: "
+            f"{app.config['SECRET_KEY']}")
