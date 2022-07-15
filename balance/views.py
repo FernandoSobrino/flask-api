a9090390fa5794b8fff18b25460d0518356e5709
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-15 21:25:28 +0200

    feat: version html de la api

--- balance/views.py
+++ balance/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify, render_template, request
 from . import app
 from .models import DBManager
 
@@ -17,35 +17,106 @@ importante versionar los endpoints (son un contrato)
 """
 
 RUTA = app.config.get("RUTA")
+db = DBManager(RUTA)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 @app.route("/api/v1/movimientos")
 def listar_movimientos():
     try:
-        db = DBManager(RUTA)
         sql = "SELECT * from movimientos ORDER BY fecha, id"
         movimientos = db.consultaSQL(sql)
-        resultado = {"status": "success", "results": movimientos}
+        if movimientos:
+            resultado = {"status": "success", "results": movimientos}
+            status_code = 200
+        else:
+            resultado = {"status": "error", "message": "No hay movimientos"}
     except Exception as error:
         resultado = {
             "status": "error",
             "message": str(error)
         }
-    return jsonify(resultado)
+        status_code = 503
+    return jsonify(resultado), status_code
 
-@app.route("/api/v1/movimientos/",methods=["POST"])
+
+@app.route("/api/v1/movimientos", methods=["POST"])
 def insertar_movimiento():
     try:
         sql = ("INSERT INTO movimientos (fecha,concepto,tipo,cantidad)"
-        "VALUES (:fecha, :concepto, :tipo, :cantidad)")
+               "VALUES (:fecha, :concepto, :tipo, :cantidad)")
         db = DBManager(RUTA)
         ha_ido_bien = db.consultaConParametros(sql, request.json)
+        if ha_ido_bien:
+            resultado = {"status": "success"}
+            status_code = 201
+        else:
+            resultado = {
+                "status": "error",
+                "message": "Error al insertar el movimiento en la base de datos"}
+            status_code = 503
+    except Exception as error:
+        resultado = {
+            "status": "error",
+            "message": str(error)
+        }
+        status_code = 404
+    return jsonify(resultado)
+
+
+@app.route("/api/v1/movimientos/<int:id>")
+def obtener_movimiento_por_id(id):
+    movimiento = db.obtenerMovimientoPorId(id)
+    if movimiento:
+        resultado = {"status": "success", "results": movimiento}
+        status_code = 200
+    else:
+        resultado = {
+            "status": "error",
+            "message": f"No he encontrado el movimiento con id {id}"
+        }
+        status_code = 404
+    return jsonify(resultado), status_code
+
+
+@app.route("/api/v1/movimientos/<int:id>", methods=['DELETE'])
+def eliminar_movimiento_por_id(id):
+        sql = "DELETE FROM movimientos WHERE id=?"
+        params = (id,)
+        ha_ido_bien = db.consultaConParametros(sql, params)
+        if ha_ido_bien:
+            status_code = 204
+            return jsonify({"status": "success"}), status_code
+        else:
+            status_code = 500
+            return jsonify(
+                {"status": "error", "message": f"Ha fallado el borrado del movimiento {id}"}
+                ), status_code
+            
+    
+
+
+@app.route("/api/v1/movimientos/<int:id>", methods=['POST'])
+def actualizar_movimiento_por_id(id):
+    try:
+        sql = "UPDATE movimientos SET {fecha,concepto,tipo,cantidad} where id=?"
+        "VALUES(:fecha , :concepto , :tipo , :cantidad)"
+        params = [id]
+        diccionario = request.json
+        for elem in diccionario:
+            params.append(diccionario[elem])
+        params = tuple(params)
+        ha_ido_bien = db.consultaConParametros(sql, params)
         if ha_ido_bien:
             resultado = {"status": "success"}
         else:
             resultado = {
                 "status": "error",
-                "message": "Error al insertar el movimiento en la base de datos"}
+                "message": "Error al modificar el movimiento en la base de datos"}
     except Exception as error:
         resultado = {
             "status": "error",
