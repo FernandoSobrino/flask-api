a1109cade2d0601a60244859ebf723436a1807ba
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-15 22:44:31 +0200

    feat: ya leemos los movimientos en la vista cliente

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -7,16 +7,30 @@ function cargarMovimientos() {
 
     peticion.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', false)
     peticion.send()
-    console.log(peticion.responseText)
+    const respuesta = JSON.parse(peticion.responseText)
+    const movimientos = respuesta.results;
 
     const tabla = document.querySelector('#cuerpo-tabla')
-    const html = '<tr><td>05/01/2022</tr>';
-    tabla.innerHTML += html;
+    let html = '';
+    for (let i = 0; i < movimientos.length; i= i+1) {
+        const mov = movimientos[i]
+        html = html + `
+        <tr>
+            <td>${mov.fecha}</td>
+            <td>${mov.concepto}</td>
+            <td>${mov.tipo}</td>
+            <td>${mov.cantidad}</td>
+        </tr>
+        `
+
+    } 
+    tabla.innerHTML = html;
 }
 
 
 
 window.onload = function () {
     const boton = document.querySelector('#boton-recarga')
-    boton.addEventListener('click', cargarMovimientos)
+    boton.addEventListener('click', cargarMovimientos);
+    cargarMovimientos();
 };
