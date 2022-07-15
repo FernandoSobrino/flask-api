26d32786415fc2936273a93b33f888fba7bc7f2d
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-15 22:04:55 +0200

    chore: cómo generar html desde JavaScript

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,6 +1,11 @@
 function cargarMovimientos() {
     console.log('Has llamado a cargar movimientos')
+
+    const tabla = document.querySelector('#cuerpo-tabla')
+    const html = '<tr><td>05/01/2022</tr>';
+    tabla.innerHTML = html;
 }
+
 
 
 window.onload = function () {
