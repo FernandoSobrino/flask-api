d00a7d51d11a79a515be47fbe04fb7c791acc55b
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-16 00:07:28 +0200

    feat: palabras ingreso/gasto

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,36 +1,59 @@
-const peticion = new XMLHttpRequest()
+let spinner;
+const peticion = new XMLHttpRequest();
 
-
-
-function cargarMovimientos() {
-    console.log('Has llamado a cargar movimientos')
-
-    peticion.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', false)
-    peticion.send()
-    const respuesta = JSON.parse(peticion.responseText)
-    const movimientos = respuesta.results;
-
-    const tabla = document.querySelector('#cuerpo-tabla')
-    let html = '';
-    for (let i = 0; i < movimientos.length; i= i+1) {
-        const mov = movimientos[i]
-        html = html + `
-        <tr>
-            <td>${mov.fecha}</td>
-            <td>${mov.concepto}</td>
-            <td>${mov.tipo}</td>
-            <td>${mov.cantidad}</td>
-        </tr>
-        `
-
-    } 
-    tabla.innerHTML = html;
+function obtenerMovimientos() {
+    spinner.classList.remove('off');
+    console.log("Obteniendo movimientos");
+    peticion.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', true);
+    peticion.send();
+    console.log("FIN de obtener movimientos");
 }
 
+function mostrarMovimientos() {
+    console.log('entramos en mostrar movimientos');
+    const tabla = document.querySelector('#cuerpo-tabla');
 
+    if (this.readyState === 4 && this.status === 200) {
+        console.log('--- TODO OK ----');
+        const respuesta = JSON.parse(peticion.responseText);
+        const movimientos = respuesta.results;
+
+
+        let html = '';
+        for (let i = 0; i < movimientos.length; i = i + 1) {
+            const mov = movimientos[i];
+            if (mov.tipo === 'G') {
+                mov.tipo = 'Gasto';
+            } else if (mov.tipo === 'I') {
+                mov.tipo = 'Ingreso';
+            } else {
+                mov.tipo = '---';
+            }
+            html = html + `
+        <tr>
+          <td>${mov.fecha}</td>
+          <td>${mov.concepto}</td>
+          <td>${mov.tipo}</td>
+          <td>${mov.cantidad}</td>
+        </tr>
+      `;
+        }
+
+        tabla.innerHTML = html;
+    } else {
+        console.error('---- Algo ha ido mal en la petición ----');
+        alert('Error al cargar los movimientos');
+    }
+    console.log('FIN de mostrar movimientos');
+    spinner.classList.add('off');
+}
 
 window.onload = function () {
-    const boton = document.querySelector('#boton-recarga')
-    boton.addEventListener('click', cargarMovimientos);
-    cargarMovimientos();
+    spinner = document.querySelector('#spinner');
+    console.log('Inicio de window.onload');
+    obtenerMovimientos();
+    peticion.onload = mostrarMovimientos;
+
+    const boton = document.querySelector('#boton-recarga');
+    boton.addEventListener('click', obtenerMovimientos);
 };
