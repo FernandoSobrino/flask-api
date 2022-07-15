c8fc986387239c8a41e10b7b1ff347f56d62a556
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-15 22:17:31 +0200

    chore: hacemos una llamada a la API desde js

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,9 +1,17 @@
+const peticion = new XMLHttpRequest()
+
+
+
 function cargarMovimientos() {
     console.log('Has llamado a cargar movimientos')
 
+    peticion.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', false)
+    peticion.send()
+    console.log(peticion.responseText)
+
     const tabla = document.querySelector('#cuerpo-tabla')
     const html = '<tr><td>05/01/2022</tr>';
-    tabla.innerHTML = html;
+    tabla.innerHTML += html;
 }
 
 
